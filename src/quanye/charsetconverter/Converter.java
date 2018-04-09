fe50dcae20d9baf32235e149cfa355d517c9f338
aizichen <1098679804@qq.com>
2018-04-09 09:26:21 +0800

    The 1.0 version

---
+++ src/quanye/charsetconverter/Converter.java
@@ -0,0 +1,126 @@
+package quanye.charsetconverter;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.apache.commons.io.FileUtils;
+
+import tools.charset.CharsetDetector;
+
+public class Converter extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel panel = new JPanel();
+	private JFileChooser chooser = new JFileChooser();
+
+	public static void main(String[] args) throws IOException {
+		new Converter().initWindow();
+	}
+
+	private void initWindow() {
+		chooser.setMultiSelectionEnabled(false);
+		chooser.setDialogTitle("第一步：请指定一个需要批量修改编码的文件夹");
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		panel.add(chooser);
+
+		int result = chooser.showOpenDialog(this);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File file = chooser.getSelectedFile();
+			add(showCharsetSelectWindow(file));
+
+			setTitle("第二步：请选择目标编码");
+			setVisible(true);
+			setBounds(500, 500, 420, 280);
+			Toolkit kit = Toolkit.getDefaultToolkit(); // 定义工具包
+			Dimension screenSize = kit.getScreenSize(); // 获取屏幕的尺寸
+			int screenWidth = screenSize.width / 2; // 获取屏幕的宽
+			int screenHeight = screenSize.height / 2; // 获取屏幕的高
+			int height = this.getHeight();
+			int width = this.getWidth();
+			setBounds(screenWidth - width / 2, screenHeight - height / 2 - 100, 380, 260);
+		}
+
+	}
+
+	/**
+	 * “选择编码”窗口
+	 * 
+	 * @return
+	 */
+	private JPanel showCharsetSelectWindow(File filePath) {
+		JPanel panel = new JPanel();
+
+		// Map<String, Charset> map = Charset.availableCharsets();
+		// String[] items = new String[map.size()];
+		// items = map.keySet().toArray(items);
+		String[] items = { "UTF-8", "GBK", };
+
+		JComboBox<String> comboBox = new JComboBox<>(items);
+
+		JLabel processing = new JLabel("");
+
+		JButton okBtn = new JButton("确定");
+		okBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				processing.setText("正在转换...");
+				try {
+					String item = (String) comboBox.getSelectedItem();
+					convert(filePath, item);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				processing.setText("转换完成");
+			}
+		});
+
+		panel.add(new JLabel("请选择需要转换的编码"));
+		panel.add(comboBox);
+		panel.add(okBtn);
+		panel.add(processing);
+		panel.setVisible(true);
+		return panel;
+	}
+
+	/**
+	 * 实现编码的转换
+	 * 
+	 * @param charSet
+	 * @throws IOException
+	 */
+	private void convert(File filePath, String charSet) throws IOException {
+		File[] files = filePath.listFiles();
+		for (File file : files) {
+			if (file.isFile()) {
+				String fileName = file.getName();
+				if (fileName.endsWith(".txt") || fileName.endsWith(".java") || fileName.endsWith(".xml")) {
+					// 转换
+					String coding = CharsetDetector.getInstance().getCharSet(file).name();
+					// 相同编码不用转
+					if (!coding.equals(charSet)) {
+						FileUtils.writeLines(file, charSet, FileUtils.readLines(file, coding));
+					}
+				}
+
+			} else {
+				convert(file, charSet);
+			}
+		}
+	}
+
+}
---
+++ src/tools/charset/CharsetDetector.java
@@ -0,0 +1,43 @@
+package tools.charset;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import info.monitorenter.cpdetector.io.ASCIIDetector;
+import info.monitorenter.cpdetector.io.ByteOrderMarkDetector;
+import info.monitorenter.cpdetector.io.CodepageDetectorProxy;
+import info.monitorenter.cpdetector.io.JChardetFacade;
+import info.monitorenter.cpdetector.io.ParsingDetector;
+
+public class CharsetDetector {
+
+	private static CharsetDetector instance;
+
+	private CharsetDetector() {
+	}
+
+	public static CharsetDetector getInstance() {
+		if (instance == null) {
+			instance = new CharsetDetector();
+		}
+		return instance;
+	}
+
+	/**
+	 * 根据文件获取其文件编码
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	@SuppressWarnings("deprecation")
+	public Charset getCharSet(File file) throws IOException {
+		CodepageDetectorProxy detector = CodepageDetectorProxy.getInstance();
+		detector.add(new ByteOrderMarkDetector());
+		detector.add(JChardetFacade.getInstance());
+		detector.add(ASCIIDetector.getInstance());
+		return detector.detectCodepage(file.toURL());
+	}
+
+}
